a0a7e4318f6811b52b9637e4f7c857f4ca81f589
The King <theking@Thes-MacBook-Air.local>
2024-06-10 18:55:41 -0500

    Inital Commit

---
+++ data.sql
@@ -0,0 +1,22 @@
+USE monger_store;
+
+INSERT INTO Products (name, image_url, price, description, category)
+VALUES
+('Beast DEF', 'images/beastdef.png', 30.00, 'The total definition of Da Digital Beast!', 'Shirt'),
+('Monger DEF', 'images/mongerdef.png', 30.00, 'The total definition of GAMEM0NGER!', 'Shirt'),
+('Beast Takeover Tank', 'images/beasttank.png', 30.00, 'Da Digital Beast takes over Gamemonger on a tank!', 'Shirt'),
+('Beast Takeover Shirt', 'images/beastshirt.png', 30.00, 'Da Digital Beast takes over Gamemonger on a shirt!!', 'Shirt'),
+('WAP WAP', 'images/wap.png', 30.00, 'Da Digital Beast favorite catchphrase on a shirt!', 'Shirt'),
+('Monger #1', 'images/number1.png', 30.00, 'There are a lot of #2s out there but only one #1!', 'Shirt'),
+('Monger Bucket Hat', 'images/buckethat.png', 22.00, 'Monger Bucket Hat!', 'Hat'),
+('Monger Beanie', 'images/beanie.png', 22.00, 'Monger Beanie Hat!', 'Hat'),
+('Monger Fuzzy Beanie', 'images/fuzzbean.png', 22.00, 'Monger Fuzzy Beanie Hat!', 'Hat'),
+('Monger Mug', 'images/mongermug.png', 10.00, 'Monger Coffee Mug!', 'Mug'),
+('Monger Sweats', 'images/mongersweat.png', 22.00, 'Monger Sweat Pants!', 'Pants'),
+('Tori Monger Sticker', 'images/torimongerstick.png', 5.00, 'Torimonger Sticker in a set of 5!', 'Sticker'),
+('Tori Monger Hype Sticker', 'images/torihypestick.png', 5.00, 'Torimonger Hype Sticker in a set of 5!', 'Sticker'),
+('Beast GG Championship Sticker', 'images/beastgg.png', 5.00, 'Da Digital Beast GG Championship Belt GG Sticker in a set of 5!', 'Sticker'),
+('GAMEM0NGER Sticker', 'images/mongerwhite.png', 5.00, 'GAMEM0NGER logo Sticker in a set of 5!', 'Sticker'),
+('GAMEM0NGER purp Sticker', 'images/mongerpurp.png', 5.00, 'GAMEM0NGER purp Sticker in a set of 5!', 'Sticker'),
+('GAMEM0NGER gold Sticker', 'images/mongergold.png', 5.00, 'GAMEM0NGER gold Sticker in a set of 5!', 'Sticker')
+
